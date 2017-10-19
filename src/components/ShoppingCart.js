9e0fdc7d82c37f9b2475b9ca436c2a0a0a79ec14
Tyler Torres <tyler.john.torres@gmail.com>
2017-10-19 13:31:13 -0600

    Added local and production ENV variables

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import 'materialize-css/dist/css/materialize.min.css'
-import CartHeader from './CartHeader';
-import AddItem from './AddItem';
-import CartItems from './CartItems';
-import CartFooter from './CartFooter';
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import CartHeader from "./CartHeader";
+import AddItem from "./AddItem";
+import CartItems from "./CartItems";
+import CartFooter from "./CartFooter";
 
 // BEFORE BUILD WITH CALLING AN API
 // class ShoppingCart extends React.Component {
@@ -35,87 +35,87 @@ import CartFooter from './CartFooter';
 // }
 
 class ShoppingCart extends React.Component {
-
   state = {
     items: [],
-    products: [],
+    products: []
+  };
 
-  }
-
+  // LOADS THE DATA FROM THE API ENDPOINT (INVOKED IMMEDIATELY AFTER A COMPONENT IS MOUNTED)
   async componentDidMount() {
-    const items = await this.getItems()
-    const products = await this.getProducts()
+    const items = await this.getItems();
+    const products = await this.getProducts();
     const productsById = products.reduce((result, product) => {
-      result[product.id] = product
-      return result
-    }, {})
+      result[product.id] = product;
+      return result;
+    }, {});
 
     this.setState({
       products: {
         byId: productsById,
         all: products
       },
-      items: items.map(item => ({ ...item, product: productsById[item.product.id] })),
-    })
+      items: items.map(item => ({
+        ...item,
+        product: productsById[item.product.id]
+      }))
+    });
   }
+  // --------------------------------------------------------------------------------------------------------
 
-// FETCH ALL OF THE ITEMS
+  // FETCH ALL OF THE ITEMS
   async getItems() {
-    const response = await fetch('http://localhost:8082/api/items')
-    const json = await response.json()
-    return json._embedded.items
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/items`);
+    const json = await response.json();
+    return json._embedded.items;
   }
+  // --------------------------------------------------------------------------------------------------------
 
-// --------------------------------------------------------------------------------------------------------
-
-
-// FETCH ALL OF THE PRODUCTS
+  // FETCH ALL OF THE PRODUCTS
   async getProducts() {
-    const response = await fetch('http://localhost:8082/api/products')
-    const json = await response.json()
-    return json._embedded.products
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/api/products`
+    );
+    const json = await response.json();
+    return json._embedded.products;
   }
-// --------------------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------------------
 
+  // POST THE DATA FROM AN ADDED ITEM TO THE SERVER
+  // THIS SAYS TO GRAB THE HREF FROM THE ITEMS LIST, THAT REFFERENCES THE PRODUCT ID
+  // OF THE SELECTED ITEM, FROM THE PRODUCTS LIST
+  itemWasAdded = async item => {
+    const response = await fetch(item.product._links.items.href, {
+      method: "POST",
+      body: JSON.stringify({ quantity: item.quantity }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    });
+    const newItem = await response.json();
+    newItem.product = this.state.products.byId[newItem.product.id];
 
-// POST THE DATA FROM AN ADDED ITEM TO THE SERVER
-  itemWasAdded = async (item) => {
-      const response = await fetch(item.product._links.items.href, {
-        method: 'POST',
-        body: JSON.stringify({ quantity: item.quantity }),
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        }
-      })
-      const newItem = await response.json()
-      newItem.product = this.state.products.byId[newItem.product.id]
-
-      this.setState({
-        items: [
-          newItem,
-          ...this.state.items,
-        ]
-      })
-    }
-// --------------------------------------------------------------------------------------------------------
-
+    this.setState({
+      items: [newItem, ...this.state.items]
+    });
+  };
+  // --------------------------------------------------------------------------------------------------------
 
   render() {
-    return (
-      this.state.items.length && this.state.products.all.length ?
-        (
-          <div>
-            <CartHeader />
-            <AddItem products={ this.state.products.all } itemAdded={ this.itemWasAdded } />
-            <CartItems items={ this.state.items } />
-            <CartFooter copyright={ this.props.copyright } />
-          </div>
-        ) :
-        (<div>Loading...</div>)
-    )
+    return this.state.items.length && this.state.products.all.length ? (
+      <div>
+        <CartHeader />
+        <AddItem
+          products={this.state.products.all}
+          itemAdded={this.itemWasAdded}
+        />
+        <CartItems items={this.state.items} />
+        <CartFooter copyright={this.props.copyright} />
+      </div>
+    ) : (
+      <div>Loading...</div>
+    );
   }
 }
 
-
-export default ShoppingCart
+export default ShoppingCart;
